bd4ccf3525e46b42d050e7c9f800fbaff21649e8
Brent Robbins <brent@variantstudios.com>
2015-07-21 21:05:03 -0600

    Updated the _config.yml file to avoid the scss file from being added to the _site directory. Tweaked the gulp deploy to compress the compiled sass (to css).

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 });
 
 /**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ * Compile files from assets/css into both _site/assets/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('sass', function () {
     return gulp.src('assets/css/**/*.scss')
@@ -44,11 +44,22 @@ gulp.task('sass', function () {
             includePaths: ['assets/css'],
             onError: browserSync.notify
         }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('assets/css'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('_site/assets/css'));
 });
+
+gulp.task('sass-deploy', function () {
+    gulp.src('assets/css/**/*.scss')
+        .pipe(sass({
+            includePaths: ['assets/css'],
+        }))
+   .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+   .pipe(sass({outputStyle: 'compressed'}))
+   .pipe(gulp.dest('assets/css'))
+   .pipe(gulp.dest('_site/assets/css'));
+});
+
 
 /**
  * Watch scss files for changes & recompile
@@ -65,4 +76,4 @@ gulp.task('watch', function () {
  */
 gulp.task('default', ['browser-sync', 'watch']);
 
-gulp.task('deploy', ['jekyll-rebuild', 'sass']);
+gulp.task('deploy', ['jekyll-build', 'sass-deploy']);
